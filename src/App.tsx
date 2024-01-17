f921a1706998ed82bdb3f7937d241268d08ee0c6
gwongibeom <k201gun@kakao.com>
2024-01-17 18:17:22 +0900

    기초적인 라우팅 + 파이어베이스 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,72 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { createGlobalStyle } from 'styled-components'
+import reset from 'styled-reset'
+import { auth } from './firebase'
+
+import LoadingScreen from './components/loading-screen'
+import Layout from './components/layout'
+import Home from './routes/Home'
+import Profile from './routes/Profile'
+import Login from './routes/Login'
+import Signup from './routes/Signup'
+import NotFound from './routes/NotFound'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+      {
+        path: 'profile',
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
+  },
+  {
+    path: '/*',
+    element: <NotFound />,
+  },
+])
+
+const GlobalStyle = createGlobalStyle`
+  ${reset},
+  * {
+    box-sizing: border-box;
+  }
+`
 
 function App() {
-  return <></>
+  const [isLoading, setLoading] = useState(true)
+
+  const init = async () => {
+    // firebase 대기
+    await auth.authStateReady()
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    init()
+  }, [])
+
+  return (
+    <>
+      <GlobalStyle />
+      {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
+    </>
+  )
 }
 
 export default App
---
+++ src/components/LoadingScreen.tsx
@@ -0,0 +1,7 @@
+export default function LoadingScreen() {
+  return (
+    <>
+      <h1>로딩</h1>
+    </>
+  )
+}
