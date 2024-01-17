f3555b87de8c4028dc30a82e9710e6fab8a3d8a6
gwongibeom <k201gun@kakao.com>
2024-01-17 19:13:41 +0900

    파베 설정

--- src/routes/CreateAccount.tsx
+++ src/routes/CreateAccount.tsx
@@ -1,98 +1,106 @@
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import { useState } from "react";
-import { auth } from "../firebase";
-import { Link, useNavigate } from "react-router-dom";
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { useState } from 'react'
+import { auth } from '../firebase'
+import { Link, useNavigate } from 'react-router-dom'
 
-import { Input, Title, Wrapper, Form, Error, Switcher } from "../components/auth-components";
-import { FirebaseError } from "firebase/app";
+import {
+  Input,
+  Title,
+  Wrapper,
+  Form,
+  Error,
+  Switcher,
+} from '../components/auth-components'
+import { FirebaseError } from 'firebase/app'
 
 const CreateAccount = () => {
-  const navigate = useNavigate();
-  const [isLoading, setLoading] = useState(false);
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  const navigate = useNavigate()
+  const [isLoading, setLoading] = useState(false)
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const {
       target: { name, value },
-    } = e;
-    if (name === "name") {
-      setName(value);
-    } else if (name === "email") {
-      setEmail(value);
-    } else if (name === "password") {
-      setPassword(value);
+    } = e
+    if (name === 'name') {
+      setName(value)
+    } else if (name === 'email') {
+      setEmail(value)
+    } else if (name === 'password') {
+      setPassword(value)
     }
-  };
+  }
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setError("");
-    if (isLoading || name === "" || email === "" || password === "") return;
+    e.preventDefault()
+    setError('')
+    if (isLoading || name === '' || email === '' || password === '') return
     try {
-      setLoading(true);
+      setLoading(true)
       // (auth: Auth, email: string, password: string)
-      const credentials = await createUserWithEmailAndPassword( // 사용자 계정 생성 -> 성공 시 자격 증명 획득 후 즉시 로그인
+      const credentials = await createUserWithEmailAndPassword(
+        // 사용자 계정 생성 -> 성공 시 자격 증명 획득 후 즉시 로그인
         auth, // 인증 인스턴스
         email, // 사용자 email
         password // 사용자 pwd
-      );
+      )
 
       // *프로필 업데이트 (닉네임 변경)
       // 업데이트 할 사용자: credentials.user
       await updateProfile(credentials.user, {
         displayName: name,
-      });
+      })
 
-      navigate("/");
+      navigate('/')
     } catch (e) {
       // 비밀번호 유효성 오류 및 이미 존재하는 계정
       if (e instanceof FirebaseError) {
-        setError(e.message);
+        setError(e.message)
       }
     } finally {
-      setLoading(false);
+      setLoading(false)
     }
-  };
+  }
   return (
     <Wrapper>
       <Title>Join</Title>
       <Form onSubmit={onSubmit}>
         <Input
           onChange={onChange}
-          name="name"
+          name='name'
           value={name}
-          placeholder="Name"
-          type="text"
+          placeholder='Name'
+          type='text'
           required
         />
         <Input
           onChange={onChange}
-          name="email"
+          name='email'
           value={email}
-          placeholder="Email"
-          type="email"
+          placeholder='Email'
+          type='email'
           required
         />
         <Input
           onChange={onChange}
           value={password}
-          name="password"
-          placeholder="Password"
-          type="password"
+          name='password'
+          placeholder='Password'
+          type='password'
           required
         />
         <Input
-          type="submit"
-          value={isLoading ? "Loading..." : "Create Account"}
+          type='submit'
+          value={isLoading ? 'Loading...' : 'Create Account'}
         />
       </Form>
-      {error !== "" ? <Error>{error}</Error> : null}
+      {error !== '' ? <Error>{error}</Error> : null}
       <Switcher>
-        Already have an account? <Link to="/login">Log in &rarr;</Link>
+        Already have an account? <Link to='/login'>Log in &rarr;</Link>
       </Switcher>
     </Wrapper>
-  );
+  )
 }
 
 export default CreateAccount
--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
-import { auth } from "../firebase";
-import { Link, useNavigate } from "react-router-dom";
-import { FirebaseError } from "firebase/app";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from 'react'
+import { auth } from '../firebase'
+import { Link, useNavigate } from 'react-router-dom'
+import { FirebaseError } from 'firebase/app'
+import { signInWithEmailAndPassword } from 'firebase/auth'
 import {
   Error,
   Form,
@@ -10,69 +10,71 @@ import {
   Switcher,
   Title,
   Wrapper,
-} from "../components/auth-components";
+} from '../components/auth-components'
+import GithubButton from '../components/github-btn'
 
 export default function CreateAccount() {
-  const navigate = useNavigate();
-  const [isLoading, setLoading] = useState(false);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  const navigate = useNavigate()
+  const [isLoading, setLoading] = useState(false)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const {
       target: { name, value },
-    } = e;
-    if (name === "email") {
-      setEmail(value);
-    } else if (name === "password") {
-      setPassword(value);
+    } = e
+    if (name === 'email') {
+      setEmail(value)
+    } else if (name === 'password') {
+      setPassword(value)
     }
-  };
+  }
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setError("");
-    if (isLoading || email === "" || password === "") return;
+    e.preventDefault()
+    setError('')
+    if (isLoading || email === '' || password === '') return
     try {
-      setLoading(true);
+      setLoading(true)
       // 로그인한 유저 정보
-      await signInWithEmailAndPassword(auth, email, password);
-      navigate("/");
+      await signInWithEmailAndPassword(auth, email, password)
+      navigate('/')
     } catch (e) {
       // 이메일/패스워드가 일치하지 않는 경우
       if (e instanceof FirebaseError) {
-        setError(e.message);
+        setError(e.message)
       }
     } finally {
-      setLoading(false);
+      setLoading(false)
     }
-  };
+  }
   return (
     <Wrapper>
       <Title>Log into 𝕏</Title>
       <Form onSubmit={onSubmit}>
         <Input
           onChange={onChange}
-          name="email"
+          name='email'
           value={email}
-          placeholder="Email"
-          type="email"
+          placeholder='Email'
+          type='email'
           required
         />
         <Input
           onChange={onChange}
           value={password}
-          name="password"
-          placeholder="Password"
-          type="password"
+          name='password'
+          placeholder='Password'
+          type='password'
           required
         />
-        <Input type="submit" value={isLoading ? "Loading..." : "Log in"} />
+        <Input type='submit' value={isLoading ? 'Loading...' : 'Log in'} />
       </Form>
-      {error !== "" ? <Error>{error}</Error> : null}
+      {error !== '' ? <Error>{error}</Error> : null}
       <Switcher>
-        Don't have an account?{" "}
-        <Link to="/create-account">Create one &rarr;</Link>
+        Don't have an account?{' '}
+        <Link to='/create-account'>Create one &rarr;</Link>
       </Switcher>
+      <GithubButton />
     </Wrapper>
-  );
+  )
 }
